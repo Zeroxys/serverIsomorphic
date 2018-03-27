a2acfcf5b8528ea19ea999570a93ecc4ff992dff
zeroxys <miguelzavalac@gmail.com>
2018-03-27 02:16:10 -0600

    optimized

--- all/actitudfem/src/client/components/body/Articles/Articles.js
+++ all/actitudfem/src/client/components/body/Articles/Articles.js
@@ -1,13 +1,57 @@
 import React from 'react'
+import LazyLoad from 'react-lazyload'
+
+const styles = {
+  article:{
+    header:{
+      padding: '10px 3% 10px 3%',
+      boxSizing: 'border-box',
+      fontSize: '25px',
+      fontFamily: ['Playfair Display', 'serif']
+    },
+    body:{
+      fontFamily: 'Arial',
+      color: '#666',
+      margin: 'margin',
+      padding: '10px 3% 10px 3%',
+    },
+    seeMore:{
+      border: '1px solid black',
+      padding: '6px 14px',
+      marginTop: '18px',
+      background: 'transparent',
+      borderRadius: '5px',
+      fontFamily: 'Helvetica',
+      fontSize: '100%' 
+    },
+    blur:{
+      backgroundImage: 'url("http://cdn2.webapps.imagendigital.com/soyactitud/images/bg_notas.png")',
+      position: 'relative',
+      zIndex: '1',
+      textAlign: 'center',
+      marginTop: '-110px',
+      height: '95px',
+      paddingTop: '40px',
+      backgroundRepeat: 'repeat-x'
+    },
+    img: {
+      width: '100%'
+    }
+  }
+}
 
 const Article = props => {
   return (
     <article>
-      <img alt={props.alt} src={props.img}/>
-      <h3>{props.title}</h3>
-      <p>{props.summary}</p>
-      <div>
-        <button>VER MÁS</button>
+      <LazyLoad throttle={200} height={300}>
+        <img alt={props.title} src={props.img}/>
+      </LazyLoad>
+      <div className={"article-content"}>
+        <h3 style={styles.article.header}>{props.title}</h3>
+        <p style={styles.article.body}>{props.summary}</p>
+      </div>
+      <div style={styles.article.blur}>
+        <button style={styles.article.seeMore}>VER MÁS</button>
       </div>
     </article>
   )
