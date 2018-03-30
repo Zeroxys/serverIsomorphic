e25c9cb21f49667e5c72a1e8017d945a115c837d
zeroxys <miguelzavalac@gmail.com>
2018-03-30 09:12:07 -0600

    working

--- all/actitudfem/src/client/actions/index.js
+++ all/actitudfem/src/client/actions/index.js
@@ -2,7 +2,7 @@ export const FETCH_POSTS = 'fetch_posts'
 export const fetchPosts = () => async (dispatch, getState, api) => {
 
   const apiKey = '22360f3a2e03f847acf5339695e42e5b'
-  const res = await api.get(`/v1/actitudfem/node.json/${apiKey}?limit=2&sort=created:DESC&fields=id%7Ctitle%7Csummary%7Curl%7Cimages%7Ctype`)
+  const res = await api.get(`/v1/actitudfem/node.json/${apiKey}?limit=1&sort=created:DESC&fields=id%7Ctitle%7Csummary%7Curl%7Cimages%7Ctype`)
   
   dispatch({
     type: FETCH_POSTS, 
